3a13e8b0976a6a90f9458c0e0ae971b6696d2f77
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-04 20:04:32 +0530

    new challenges

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactPageContent: contactContent});
 });
-
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
 
 
