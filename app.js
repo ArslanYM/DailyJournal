088cda831e483656fb9c3d6b5f603b5dc18fb96a
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-07 22:10:43 +0530

    challenge8

--- app.js
+++ app.js
@@ -27,9 +27,13 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactPageContent: contactContent});
 });
+
 app.get("/compose", function(req, res){
   res.render("compose");
 });
+ app.post("/compose", function(req, res){
+    console.log(req.body.journal);
+ });
 
 
 
